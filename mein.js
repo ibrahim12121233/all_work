2f13855b99571e57b4567433cdadb59e17a31344
ibrahim <nsism99@gmail.com>
2023-08-16 15:30:44 +0300

    done

---
+++ mein.js
@@ -0,0 +1,165 @@
+// مثال بسيط
+
+// let number = 100;
+// if (number <= 100){
+// console.log("yes"); }
+
+// else {
+// console.log(no);}
+
+// // مقدمة في الجملة الشرطية
+
+// let name = "ibrahim";
+// let age = 22;
+
+// if (age > 20 && name =="ibrahim"){
+//     console.log("welcome back ibrahim");
+// }
+// else{
+//     console.log("this is the ronge acount");
+// }
+
+// let numbers = 199;
+
+// if (numbers > 200){
+//     console.log("yes");
+// }else if (number > 80){
+//     console.log("vamos");
+// }
+
+
+// // درجة طلاب
+// let ibrahim = window.prompt("enter your grade");
+// if (ibrahim >= 95 && ibrahim<=100){
+//     console.log("A+");
+//     alert("A+")
+// }else if (ibrahim >= 90 && ibrahim<=94){
+//     console.log("b+");
+//     alert("b+")
+// }else if (ibrahim >=80 && ibrahim<=89){
+//     console.log("b");
+//     alert("b")
+// }else if (ibrahim >=75 && ibrahim<=79){
+//     console.log("c");
+//     alert("c")
+// }else if (ibrahim >=60 && ibrahim<=69){
+//     console.log("D");
+//     alert("d")
+// }else if (ibrahim >=50 && ibrahim<=59){
+//     console.log("F");
+//     alert("f")
+// }else{
+//     alert("this is ronge");
+// }
+
+// let speed=window.prompt("enter your speed");
+// let speedlimi=window.prompt("enter your speed limit");
+
+// let massege= speed>speedlimi?"انت تقود بسرعة هائلة":"انت تقود بامان"
+
+// alert(massege)
+
+// let monthNumber = 22;
+// let monthName;
+
+// switch (monthNumber){
+//     case 1:
+//         monthName="يناير";
+//         break;
+//     case 2:
+//         monthName="فبراير";
+//         break;
+//     case 3:
+//         monthName="مارس";
+//         break;
+//     case 4:
+//         monthName="ابريل";
+//         break;
+//     case 5:
+//         monthName="مايو";
+//         break;
+//     case 6:
+//         monthName="يوينو";
+//         break;
+//     case 7:
+//         monthName="يوليو";
+//         break;
+//     case 8:
+//         monthName="اغسطس";
+//         break;
+//     case 9:
+//         monthName="سبتمبر";
+//         break;
+//     case 10:
+//         monthName="اكتوبر";
+//         break;
+//     case 11:
+//         monthName="نوفمبر";
+//         break;
+      
+//     case 12:
+//         monthName="ديسمبر";
+//         break;    
+//     default:
+//         monthName="شهر غير صالح";
+// }
+// alert( " رقم الشهر"+monthNumber)
+// alert("  اسم الشهر"+monthName)
+
+
+
+
+// switch (monthNumber){
+//     case 1:
+//         monthName=0;
+//         break;
+//     case 2:
+//         monthName=55;
+//         break;
+//     case 3:
+//         monthName=57;
+//         break;
+//     case 4:
+//         monthName=78;
+//         break;
+//     case 5:
+//         monthName=99;
+//         break;
+//     case 6:
+//         monthName=38;
+//         break;
+//     case 7:
+//         monthName=1;
+//         break;
+//     case 8:
+//         monthName=2;
+//         break;
+//     case 9:
+//         monthName=3;
+//         break;
+//     case 10:
+//         monthName=4;
+//         break;
+//     case 11:
+//         monthName=5;
+//         break;
+      
+//     case 12:
+//         monthName=7;
+//         break;    
+//     default:
+//         monthName="شهر غير صالح";
+// }
+// alert("رقم الشهر"+monthNumber)
+// alert("اسم الشهر"+monthName)
+
+// console.log("23string"==23number);
+// console.log("23string"===23number);
+
+// let logg = false;
+
+// if(!logg){
+//     console.log("hi");
+// }else{
+//     console.log("cis");
+// }
